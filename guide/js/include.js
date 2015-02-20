3be330d78e19ec598a303d3a85fa1403a0bf61b9
luohara <luciene.ohara@tricae.com.br>
2015-02-20 19:00:27 -0200

    Include.js

---
+++ guide/js/include.js
@@ -0,0 +1,6 @@
+$( document ).ready(function() {
+    $("#menuLateral").load("_partial/menu-lateral.html");
+    $("#header").load("_partial/header.html");
+    $("#menuList").load("_partial/menu-list.html");
+});
+
