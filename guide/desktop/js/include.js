713396be2b344210a2f63a3f7f1920edd2eaed52
luohara <luciene.ohara@tricae.com.br>
2015-02-20 19:08:07 -0200

    Teste home

--- guide/desktop/js/include.js
+++ guide/desktop/js/include.js
@@ -1,4 +1,4 @@
-$(function(){
+$( document ).ready(function() {
     $("#menuLateral").load("_partial/menu-lateral.html");
     $("#header").load("_partial/header.html");
     $("#menuList").load("_partial/menu-list.html");
--- guide/mobile/js/include.js
+++ guide/mobile/js/include.js
@@ -1,5 +1,6 @@
-$(function(){
+$( document ).ready(function() {
     $("#menuLateral").load("_partial/menu-lateral.html");
     $("#header").load("_partial/header.html");
     $("#menuList").load("_partial/menu-list.html");
 });
+
