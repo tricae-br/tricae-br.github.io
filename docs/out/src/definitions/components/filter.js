7f8ef0c5b3781a794f8bd29a3b3f402ca71cd19a
luohara <luciene.ohara@tricae.com.br>
2015-02-20 18:49:47 -0200

    Home

--- docs/out/src/definitions/components/filter.js
+++
@@ -1,7 +0,0 @@
-$( document ).ready(function() {
-    $('.filter-box input:checked').parent().parent().addClass('active');
-
-    $('.filter-box input').click(function(){
-        $(this).parent().parent().toggleClass('active');
-    });
-});
