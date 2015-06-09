6b72a4830fcb15fa812fa91c02033ff4ef104f7e
luohara <luciene.ohara@tricae.com.br>
2015-06-09 16:57:52 -0300

    Sidebar script fix

--- guide/mobile/js/include.js
+++ guide/mobile/js/include.js
@@ -2,5 +2,9 @@ $( document ).ready(function() {
     $("#menuLateral").load("http://tricae-br.github.io/guide/mobile/partial/menu-lateral.html");
     $("#header").load("http://tricae-br.github.io/guide/mobile/partial/header.html");
     $("#menuList").load("http://tricae-br.github.io/guide/mobile/partial/menu-list.html");
+
+    //Menu
+    $('#menuLateral').show();
+    $('#menuLateral').mmenu();
 });
 
