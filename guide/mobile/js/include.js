b222bfa7f1915cdab5e034de2aa67152bfc3711b
luohara <luciene.ohara@tricae.com.br>
2015-06-09 14:29:47 -0300

    Include url changed to absolute url

--- guide/mobile/js/include.js
+++ guide/mobile/js/include.js
@@ -1,6 +1,6 @@
 $( document ).ready(function() {
-    $("#menuLateral").load("/guide/mobile/partial/menu-lateral.html");
-    $("#header").load("/guide/mobile/partial/header.html");
-    $("#menuList").load("/guide/mobile/partial/menu-list.html");
+    $("#menuLateral").load("http://tricae-br.github.io/guide/mobile/partial/menu-lateral.html");
+    $("#header").load("http://tricae-br.github.io/guide/mobile/partial/header.html");
+    $("#menuList").load("http://tricae-br.github.io/guide/mobile/partial/menu-list.html");
 });
 
