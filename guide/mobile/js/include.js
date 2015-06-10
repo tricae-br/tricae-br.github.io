070d60284597d3f51c0c542db21ddb5d15ade7d2
luohara <luciene.ohara@tricae.com.br>
2015-06-10 11:50:31 -0300

    Fixed sidebar and header layout for mobile

--- guide/mobile/js/include.js
+++ guide/mobile/js/include.js
@@ -1,5 +1,5 @@
 $( document ).ready(function() {
     $("#menuLateral").load("http://tricae-br.github.io/guide/mobile/partial/menu-lateral.html");
-    $("#header").load("http://tricae-br.github.io/guide/mobile/partial/header.html");
+    //$("#header").load("http://tricae-br.github.io/guide/mobile/partial/header.html");
 });
 
